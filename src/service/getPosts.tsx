2be7ba2bad5d6114973383eaf21097b164d4227d
chosule <89799325+chosule@users.noreply.github.com>
2023-12-07 00:51:53 +0900

    fix: 동적 라우팅 해보기

--- src/service/getPosts.tsx
+++ src/service/getPosts.tsx
@@ -1,20 +1,26 @@
 import { promises } from "fs";
 import path from "path";
+import { sync } from "glob";
 
 type Posts = {
-    id:string;
-    name:string;
-}
-export async function getPosts():Promise<Posts[]> {
-    const filePath = path.join(process.cwd(),'data','blog.json');
+  id: string;
+  name: string;
+};
+const BASE_PATH = "/post";
 
-    const data = await promises.readFile(filePath, "utf-8");
-    
-    return JSON.parse(data);
+const POSTS_PATH = path.join(process.cwd(), BASE_PATH);
+
+export async function getPosts(): Promise<Posts[]> {
+  // 모든 파일을 검색하도록 변경
+  const postPaths = sync(`${POSTS_PATH}/**/*.*`);
+  return postPaths.map((path) => {
+    return {
+      slug: path.slice(path.indexOf(BASE_PATH)).replace(),
+    };
+  });
 }
 
-export async function getPost(id:string): Promise<Posts | undefined>{
-    const products = await getPosts();
-    return products.find((item) => item.id === id);
+export async function getPost(id: string): Promise<Posts | undefined> {
+  const products = await getPosts();
+  return products.find((item) => item.id === id);
 }
-
