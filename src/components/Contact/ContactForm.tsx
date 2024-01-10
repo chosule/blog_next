19d80a0ba9fc257c40d858f550f7b515305ffb70
chosule <89799325+chosule@users.noreply.github.com>
2024-01-11 00:34:58 +0900

    🙆🏻‍♀️: nodeMailer 메세지 전송 완료

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -1,76 +1,106 @@
-'use client';
+"use client";
 import useWatchTimeout from "@/lips/useWatchTimeout";
+import { sendContactEmail } from "@/service/contact";
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
-import {toast} from "react-hot-toast";
+import { toast } from "react-hot-toast";
 
-type Form ={
-    from:string;
-    subject:string;
-    message:string;
-}
+type Form = {
+  from: string;
+  subject: string;
+  message: string;
+};
+
+const DEFAULT_DATA = {
+  from: "",
+  subject: "",
+  message: "",
+};
 export default function ContactForm() {
-    const [form, setForm] = useState<Form>({
-        from:"",
-        subject:"",
-        message:""
-    });
-    
-    const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const [form, setForm] = useState<Form>(DEFAULT_DATA);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
-    setForm((prev) => ({...prev, [name]: value}));
-    }
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
 
-    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        toast('전송에 성공하였습니다 !',{icon:"✨",style:{background:"#f0f8ff"}});
-        console.log('form?', form);
-     }
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    sendContactEmail(form) //
+      .then(() => {
+        toast("전송에 성공하였습니다 !", {
+          icon: "✨",
+          style: { background: "#f0f8ff" },
+        });
+        setForm(DEFAULT_DATA);
+      })
+      .catch((err) => {
+        console.log("err", err);
+        toast("전송에 실패하였습니다. 다시 시도해주세요.", {
+          icon: "😢",
+          style: { background: "#f0f8ff" },
+        });
+      });
+  };
 
-     return(
+  return (
+    <div className="flex flex-col gap-5">
+      <h1 className="suit text-center text-2xl">Or Send me an email</h1>
+      <form
+        onSubmit={onSubmit}
+        className="flex h-full flex-col justify-evenly gap-5"
+      >
         <div className="flex flex-col gap-5">
-            <h1 className="suit text-2xl text-center">Or Send me an email</h1>
-            <form onSubmit={onSubmit} className="h-full flex flex-col gap-5 justify-evenly">
-                <div className="flex flex-col gap-5">
-                    <div className="flex gap-8 relative">
-                        <label htmlFor="from" className="suit text-lg">your email ? 👀</label>
-                        <input
-                            className="grow bg-[aliceblue]  border-[#f6f6f6] rounded"
-                            type="text"
-                            id="from" 
-                            name="from" 
-                            required 
-                            autoFocus 
-                            value={form.from} 
-                            onChange={onChange}
-                        />
-                    </div>
-                    <div className="flex gap-8 relative">
-                        <label htmlFor="subject" className="suit text-lg">subject</label>
-                        <input
-                            className="grow bg-[aliceblue] rounded"
-                            type="text" 
-                            id="subject"
-                            name="subject" 
-                            required 
-                            value={form.subject} 
-                            onChange={onChange}
-                        />
-                    </div>
-                    <div className="flex gap-8 relative">
-                        <label htmlFor="message" className="suit text-lg">message 📃</label>
-                        <textarea
-                            className="grow bg-[aliceblue] rounded resize-none"
-                            rows={10}
-                            id="message" 
-                            name="message" 
-                            required 
-                            value={form.message} 
-                            onChange={onChange}
-                        />
-                    </div>
-                </div>
-                <button type="submit" className="suit bg-[aliceblue] mx-auto w-32 h-10 rounded-lg">전송하기</button>
-            </form>
+          <div className="relative flex gap-8">
+            <label htmlFor="from" className="suit text-lg">
+              your email ? 👀
+            </label>
+            <input
+              className="grow rounded  border-[#f6f6f6] bg-[aliceblue]"
+              type="text"
+              id="from"
+              name="from"
+              required
+              autoFocus
+              value={form.from}
+              onChange={onChange}
+            />
+          </div>
+          <div className="relative flex gap-8">
+            <label htmlFor="subject" className="suit text-lg">
+              subject
+            </label>
+            <input
+              className="grow rounded bg-[aliceblue]"
+              type="text"
+              id="subject"
+              name="subject"
+              required
+              value={form.subject}
+              onChange={onChange}
+            />
+          </div>
+          <div className="relative flex gap-8">
+            <label htmlFor="message" className="suit text-lg">
+              message 📃
+            </label>
+            <textarea
+              className="grow resize-none rounded bg-[aliceblue]"
+              rows={10}
+              id="message"
+              name="message"
+              required
+              value={form.message}
+              onChange={onChange}
+            />
+          </div>
         </div>
-    )
+        <button
+          type="submit"
+          className="suit mx-auto h-10 w-32 rounded-lg bg-[aliceblue]"
+        >
+          전송하기
+        </button>
+      </form>
+    </div>
+  );
 }
--- src/service/email.tsx
+++ src/service/email.tsx
@@ -1,5 +1,32 @@
+import nodemailer from "nodemailer";
+
 export type EmailData = {
-    from:string;
-    subject:string;
-    message:string;
+  from: string;
+  subject: string;
+  message: string;
+};
+
+export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.AUTH_USER,
+    pass: process.env.AUTH_PASS,
+  },
+});
+
+export async function sendEmail({ subject, from, message }: EmailData) {
+  const mailData = {
+    to: process.env.AUTH_USER,
+    subject: `[BLOG] ${subject}`,
+    from,
+    html: `
+        <h3>${subject}</h3>
+        <p>${message}</p>
+        <br/>
+        <p>보낸사람:${from}</p>
+        `,
+  };
+  return transporter.sendMail(mailData);
 }
