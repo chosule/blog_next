ddcca78292ed4e782043ad3624aa9d8fdad32cdb
chosule <89799325+chosule@users.noreply.github.com>
2024-01-17 00:33:19 +0900

    💅 : mdx 파일 스타일 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,8 @@
 import type { Config } from "tailwindcss";
 
 /** @type {import('tailwindcss').Config} */
-module.exports ={
-  darkMode: 'class',
+module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
@@ -17,43 +17,44 @@ module.exports ={
         suit: ["--font-suit"],
       },
       colors: {
-        neutral:{
-          50:"#fafafa",
-          100:"#E4E6E8",
-          140:"#a9a9a9",
-          145:"#6b7280",
+        neutral: {
+          50: "#fafafa",
+          100: "#E4E6E8",
+          140: "#a9a9a9",
+          145: "#6b7280",
+          148: "#e9f5fa",
           150: "#f0f8ff",
           350: "#9EDDFF",
-          500:"#1f2e3d",
-          550:"#1e90ff",
+          500: "#1f2e3d",
+          550: "#1e90ff",
           600: "#0000ff",
-          800:"#ffe716",
-          850:"#202428",
+          800: "#ffe716",
+          850: "#202428",
           900: "#000",
-        }
+        },
       },
     },
   },
   variants: {
     typography: ["dark"],
   },
-  plugins: [require("@tailwindcss/typography"),
-          ({addComponents}:{addComponents:any}) => {
-            addComponents({
-              '.text-primary':{
-                '@apply text-neutral-850 dark:text-neutral-50' : '',
-              },
-              '.bg-primary':{
-                '@apply bg-neutral-50 dark:bg-neutral-850' : '',
-              },
-              '.bg-secondary':{
-                '@apply bg-neutral-100 dark:bg-neutral-100' : '',
-              },
-              '.toggle-color':{
-                '@apply bg-neutral-550 dark:bg-neutral-140' : '',
-              },
-            })
-          }
+  plugins: [
+    require("@tailwindcss/typography"),
+    ({ addComponents }: { addComponents: any }) => {
+      addComponents({
+        ".text-primary": {
+          "@apply text-neutral-850 dark:text-neutral-50": "",
+        },
+        ".bg-primary": {
+          "@apply bg-neutral-50 dark:bg-neutral-850": "",
+        },
+        ".bg-secondary": {
+          "@apply bg-neutral-100 dark:bg-neutral-100": "",
+        },
+        ".toggle-color": {
+          "@apply bg-neutral-550 dark:bg-neutral-140": "",
+        },
+      });
+    },
   ],
-}
-
+};
