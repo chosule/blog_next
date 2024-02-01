28a614a164f07433bfa7854624ceaa8154ef7eb6
unknown <chosule@naver.com>
2024-02-01 13:51:47 +0900

    💅 : contact 페이지 디자인 수정

--- src/components/Contact/ContactMe.tsx
+++ src/components/Contact/ContactMe.tsx
@@ -8,38 +8,43 @@ export default function ContactMe() {
   return (
       <div className="flex flex-col items-center gap-5">
         <SubTitle>✨ Contact me</SubTitle>
-        <ul className="relative flex h-[200px] w-[439px] flex-col items-center justify-center gap-2 overflow-hidden bg-cover drop-shadow-2xl">
-          <li className="md:background-item text-center italic md:bg-[url('/Image/wave.jpeg')] opacity-[.6]"></li>
-          <li className="text-center italic font-semibold">
-            <p>phone </p>
-            <p>010-9724-3290</p>
+        <ul className="flex h-[200px] w-[439px] flex-col gap-3 overflow-hidden bg-cover rounded-xl bg-neutral-50 p-8 contact-me-shadow">
+          <li className="">
+            <h3 className="font-bold text-lg dark:text-neutral-900">김초슬</h3>
+            <h3 className="text-xs dark:text-neutral-900">FrontEnd</h3>
           </li>
-          <li className="flex items-center justify-evenly gap-2 text-center italic font-semibold">
-            <p>Github </p>
-            <Link
-              target="_blank"
-              href="https://github.com/chosule"
-              className="text-xl"
-            >
-              <FaGithub />
-            </Link>
-          </li>
-          <li className="flex items-center justify-evenly gap-2 text-center italic font-semibold">
-            <p>Blog </p>
-            <Link target="_blank" href="#" className="text-xl">
-              <SiBloglovin />
-            </Link>
-          </li>
-          <li className="flex items-center justify-evenly gap-2 text-center italic font-semibold">
-            <p>Velog </p>
-            <Link
-              target="_blank"
-              href="https://velog.io/@chosule/posts"
-              className="text-xl"
-            >
-              <SiVelog />
-            </Link>
-          </li>
+          <ul className="grid grid-cols-3 gap-2">
+            <li className="text-sm flex mt-4 col-start-1 col-end-4">
+              <p className="dark:text-neutral-900">Mobile. </p>
+              <p className="dark:text-neutral-900"> 010-9724-3290</p>
+            </li>
+            <li className="flex gap-2 text-xs items-center">
+              <p className="dark:text-neutral-900">Github </p>
+              <Link
+                target="_blank"
+                href="https://github.com/chosule"
+                className="text-xl text-neutral-900"
+              >
+                <FaGithub className="text-md"/>
+              </Link>
+            </li>
+            <li className="flex items-center gap-2 text-xs">
+              <p className="dark:text-neutral-900">Blog </p>
+              <Link target="_blank" href="#" className="text-xl text-neutral-900">
+                <SiBloglovin className="dark:bg-neutal-900"/>
+              </Link>
+            </li>
+            <li className="flex items-center gap-2 text-xs">
+              <p className="dark:text-neutral-900">Velog </p>
+              <Link
+                target="_blank"
+                href="https://velog.io/@chosule/posts"
+                className="text-xl dark:text-neutral-900"
+              >
+                <SiVelog />
+              </Link>
+            </li>
+          </ul>
         </ul>
       </div>
 
