784e50682a59996b9404db75d205c495fd97710f
chosule <89799325+chosule@users.noreply.github.com>
2024-02-14 02:27:12 +0900

    🛠 : sitemap 삭제

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,3 +3,15 @@ module.exports = {
   siteUrl: "https://chosule-blog.vercel.app",
   generateRobotsTxt: true, // (optional)
 };
+
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: "http://localhost:8088", // .게시하는 site의 url
+  generateRobotsTxt: true, // robots.txt generate 여부 (자동생성 여부)
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      `${process.env.NEXT_PUBLIC_BASE_URL}/post-sitemap.xml`,
+    ],
+  }, // robots.txt 옵션 설정
+};
