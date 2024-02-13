92ea6959976b3b9e997359f708507bdff28972ff
chosule <89799325+chosule@users.noreply.github.com>
2024-02-14 01:24:50 +0900

    🛠 : site-map 수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://chosule-blog.vercel.app/",
+  siteUrl: "https://chosule-blog.vercel.app",
   generateRobotsTxt: true, // (optional)
 };
